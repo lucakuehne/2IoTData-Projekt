6664cf0967b9047d4dc88ffbdc2129562f45368f
Luca K <luca.kuehne@gmail.com>
2024-06-05 19:54:09 +0200

    fix import

--- src/routes/api/door-sensor/+server.js
+++ src/routes/api/door-sensor/+server.js
@@ -1,7 +1,7 @@
 import { json } from '@sveltejs/kit'
 import { MongoClient } from 'mongodb'
 import { env } from '$env/dynamic/private'
-var Gpio = require('onoff').Gpio;
+import Gpio from 'onoff'
 
 const dbClient = new MongoClient(env.DB_URI);
 
