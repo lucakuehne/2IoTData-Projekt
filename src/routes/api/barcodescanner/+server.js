a775eaa9c6206e12402e404f4f09a3b6cb213560
Luca K <luca.kuehne@gmail.com>
2024-06-05 22:43:14 +0200

    .

--- src/routes/api/barcodescanner/+server.js
+++ src/routes/api/barcodescanner/+server.js
@@ -6,7 +6,26 @@ const dbClient = new MongoClient(env.DB_URI);
 
 export async function POST({ request }) {
     let scan = await request.json()
-    let productName = scan.productName
-
-    console.log(productName)
+    //console.log(scan.productName)
+    try {
+        await dbClient.connect();
+        const database = dbClient.db('2iotdata');
+        const scansCollection = database.collection('scans');
+            let scan = await scansCollection.insertOne({
+                productName: scan.productName,
+                fetched: false
+            });
+            return json({
+                status: 200,
+                body: scan
+            });
+    } catch (exceptionVar) {
+        console.log(exceptionVar);
+        return json({ 
+            status: 500 
+        });
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await dbClient.close();
+    }
 }
